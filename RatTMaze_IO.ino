a46315690cfb320472dfc1188070169cff666585
BaileyGlen <baileyglen@gmail.com>
2016-03-02 16:02:14 -0500

    Lots of code added

--- RatTMaze_IO.ino
+++ RatTMaze_IO.ino
@@ -6,16 +6,36 @@
 
 // Included code in the servo library and the 
 // definition for the Door Struct
-
 #include "doorDef.h"  // built into arduino
 #include <Servo.h>    
 
 // Declaration and Initiation of 3 Doors. Here, adjustments
 // can be made to the pins, default doors states, etc
-Door rightDoor = {8,  11, true, 45, 135, 90,0,0};
-Door leftDoor  = {9,  12, true, 45, 135, 90,0,0};
-Door homeDoor  = {10, 13, true, 45, 135, 90,0,0};
+/////// Adjust Door Parameters here!!!///////////////////
+// For instance, the door ranges are the 4th and 5th parameters below
+// The In and Out Pins are the 1st and second parameters below
+Door rightDoor = {8,  11, true, 45, 135, 90, millis()};
+Door leftDoor  = {9,  12, true, 45, 135, 90, millis()};
+Door homeDoor  = {10, 13, true, 45, 135, 90, millis()};
 
+// Declaration of the 3 pump pins here
+// We only need to declare inputs for the pumps. The relays
+// themselves are the outputs
+// The seedstudio relay shield v2.0 uses the following layout
+// Digital 4 – controls RELAY4’s COM4 pin (located in J4)
+// Digital 5 – controls RELAY3’s COM3 pin (located in J3)
+// Digital 6 – controls RELAY2’s COM2 pin (located in J2)
+// Digital 7 – controls RELAY1’s COM1 pin (located in J1)
+// We are using ...
+// rightpump - J4, pin 4
+// leftpump  - J3, pin 5
+// homepump  - J2, pin 6
+int rightPumpPinIn = 4;
+int leftPumpPinIn  = 5;
+int homePumpPinIn  = 6;
+
+// This variable will be used to handle the delay time
+int doorMs = 30; 
 
 void setup() 
 {
@@ -27,12 +47,16 @@ void setup()
   // }
   //  Serial.println("Hello");
 
-
   // Set Input and output pins for pumps and doors
+  // Doors
   pinMode(rightDoor.pinIn, INPUT);
   pinMode(leftDoor.pinIn, INPUT);
   pinMode(homeDoor.pinIn, INPUT);
-
+  // Pumps
+  pinMode(rightPumpPinIn, INPUT);
+  pinMode(leftPumpPinIn, INPUT);
+  pinMode(homePumpPinIn, INPUT);
+  
   //Attach the servos to the door pin outpus
   rightDoor.servo.attach(rightDoor.pinOut);
   leftDoor.servo.attach(leftDoor.pinOut);
@@ -47,33 +71,39 @@ void setup()
 void loop() 
 {  
   // check the position of each door and update its positions
+  
   checkDoor(rightDoor);
   checkDoor(leftDoor);
   checkDoor(homeDoor);
+
+  // No need to do anything witht he pumps, its just running through the arduino inputs.
+  
 }
 
 
 void checkDoor(Door &curDoor)
 {
   // Check if the door is set to be closed...
-  if (digitalRead (curDoor.pinIn)==LOW)
+  if ((digitalRead (curDoor.pinIn)==LOW) && ((unsigned long)(millis() - curDoor.prevMs)  >= doorMs))
   {
     // if it should be closed, but isn't at the closed position...
-    if (curDoor.curPos != curDoor.minPos)
+    if (curDoor.curPos != curDoor.minPos) 
     {
       // ...move towards the close position
-      curDoor.curPos--;
+      curDoor.curPos = curDoor.curPos - (1*curDoor.dir);
       curDoor.servo.write(curDoor.curPos);
+      curDoor.prevMs = millis();
     }
   }
 
   // same as above, but for when the door should be open
   else 
   {
-    if (curDoor.curPos != curDoor.maxPos)
+    if ((curDoor.curPos != curDoor.maxPos) && ((unsigned long)(millis() - curDoor.prevMs)  >= doorMs))
     {
-      curDoor.curPos++;
+      curDoor.curPos = curDoor.curPos + (1*curDoor.dir);
       curDoor.servo.write(curDoor.curPos);
+      curDoor.prevMs = millis();
     }
   }
 }
--- doorDef.h
+++ doorDef.h
@@ -1,13 +1,21 @@
+// W. Bailey Glen Jr.
+// 03/02/2016
+// Arduino 1.6.7
+// Arduino Uno Rev 3 
+
 #include <Servo.h>
+
+// Creates a door object for passing data around
 struct door
 {
-  int pinIn;
-  int pinOut;
-  bool dir; // True = normal
-  int minPos;
-  int maxPos;
-  int curPos;
-  long nextMoveMs;
-  Servo servo;
+  int pinIn;    // pin on the ardion which will receive the door state
+  int pinOut;   // pin on the arduino which will drive the door servo
+  bool dir;     // True = normal
+  int minPos;   // maxiumm close before hitting the wall 
+  int maxPos;   // maximum open before hitting the wall
+  int curPos;   // current position from 0 to 180
+  unsigned long prevMs;  // used for door speed control
+  Servo servo;  // generates a servo from servo library
 };
+// makes "Door = door" ignore this
 typedef struct door Door;
