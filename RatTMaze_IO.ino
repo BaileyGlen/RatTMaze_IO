02fdde839c4f80f7b33ac64163a1817975816258
BaileyGlen <baileyglen@gmail.com>
2016-03-02 11:33:26 -0500

    working version 1

--- RatTMaze_IO.ino
+++ RatTMaze_IO.ino
@@ -24,60 +24,48 @@ int pos = 0;    // variable to store the servo position
 //const int rightPumpMinPos = 45;
 //const int rightPumpMaxPos = 135;
 //const int rightPump
-Door rightDoor;
-Door leftDoor;
-Door homeDoor;
-
+Door rightDoor = {2, 5, true, 45, 135, 90,0,0};
+Door leftDoor = {3, 6, true, 45, 135, 90,0,0};
+Door homeDoor = {4, 7, true, 45, 135, 90,0,0};
 void setup() 
 {
-  Serial.begin(115200);
+ Serial.begin(115200);
+   while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println("Hello");
 //  handshakeNeeded = true;
 //  Serial.println ("Initializing port:");
   // mkpump(Input Pin, Output Pin, Direction, minPos, maxPos); 
-  Door rightDoor = {1, 4, true, 45, 135, 45,0,0};
-  Door leftDoor = {2, 5, true, 45, 135, 45,0,0};
-  Door homeDoor = {3, 6, true, 45, 135, 45,0,0};
-
- 
+//  Door rightDoor = {2, 5, true, 45, 135, 90,0,0,};
+//  Door leftDoor = {3, 6, true, 45, 135, 90,0,0};
+//  Door homeDoor = {4, 7, true, 45, 135, 90,0,0};
   pinMode(rightDoor.pinIn, INPUT);
   pinMode(leftDoor.pinIn, INPUT);
   pinMode(homeDoor.pinIn, INPUT);
   rightDoor.servo.attach(rightDoor.pinOut);
   leftDoor.servo.attach(leftDoor.pinOut);
   homeDoor.servo.attach(homeDoor.pinOut);
-
-  checkDoor(rightDoor);
   // for tesitng
-  pinMode(13,OUTPUT);
-//  pinMode(homePumpIn, INPUT);
-//  pinMode(rightPumpIn, INPUT);
-//  pinMode(leftPumpIn, INPUT);
-//  pinMode(homePumpOut, OUTPUT);
-//  pinMode(rightPumpOut, OUTPUT);
-//  pinMode(leftPumpOut, OUTPUT);
+  rightDoor.servo.write(rightDoor.curPos);
+  leftDoor.servo.write(leftDoor.curPos);
+  homeDoor.servo.write(homeDoor.curPos);
 }
 
 void loop() 
-{
+{  
   checkDoor(rightDoor);
   checkDoor(leftDoor);
   checkDoor(homeDoor);
   //testing
-  if (rightDoor.nextMoveMs==0)
-  {
-    digitalWrite(13,HIGH);
-  }else
-  {
-    digitalWrite(13,LOW);
-  }
-  
+
 }
 
 
 
 void checkDoor(Door &curDoor)
 {
-  
+  //testing
   if (digitalRead (curDoor.pinIn)==LOW)
   {
     curDoor.curState==false;
@@ -90,23 +78,13 @@ void checkDoor(Door &curDoor)
   else 
   {
     curDoor.curState==true;
-    if (curDoor.curPos != curDoor.minPos)
+   
+    if (curDoor.curPos != curDoor.maxPos)
     {
       curDoor.curPos++;
       curDoor.servo.write(curDoor.curPos);
     }
   }
-  //testing
-  if ( ((int)(millis()/1000) % 2)==0)
-  {
-    curDoor.nextMoveMs=0;
-  }
-  else 
-  {
-    curDoor.nextMoveMs=1;
-  }
-  // check the state of the pump Pin
-  // If there is a change in the Pin
 }
 
 
